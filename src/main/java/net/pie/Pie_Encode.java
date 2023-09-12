a2c7c8dd3579885d769ffb0ed38e66edc3d4af36
helcat1 <terry.clarke@gmail.com>
2023-09-12 17:10:42 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/net/pie/Pie_Config.java

---
+++ src/main/java/net/pie/Pie_Encode.java
@@ -0,0 +1,79 @@
+package net.pie;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+
+public class Pie_Encode {
+    private Pie_Config config;
+
+    /*************************************************
+     * Start
+     *************************************************/
+    public Pie_Encode(Pie_Config config, String toBeEncrypted) {
+        encode(toBeEncrypted);
+    }
+    public Pie_Encode(String toBeEncrypted) {
+        encode(toBeEncrypted);
+    }
+
+    /*************************************************
+     * encode
+     *************************************************/
+    public BufferedImage encode(String toBeEncrypted) {
+        for (int i = 0; i < ((toBeEncrypted == null) ? "" : toBeEncrypted).length() % getConfig().getUse().getNumber(); i++)
+            toBeEncrypted = toBeEncrypted + " ";
+
+        byte[] originalArray = Objects.requireNonNull(toBeEncrypted).getBytes(StandardCharsets.UTF_8);
+
+        double dimension = (int) Math.sqrt((double) originalArray.length / getConfig().getUse().getNumber());
+
+        Integer r = null;
+        Integer g = null;
+        Integer b = null;
+        List<Color> list = new ArrayList<Color>();
+        for (int i : originalArray) {
+            if (r == null) {
+                r = i;
+            } else if (g == null) {
+                g = i;
+            } else if (b == null) {
+                b = i;
+                list.add(createColor(r,g,b));
+                r = null;
+                g = null;
+                b = null;
+            }
+        }
+
+        BufferedImage buffImg = new BufferedImage((int) dimension, (int) dimension, BufferedImage.TYPE_INT_ARGB);
+        Integer count = 0;
+        for (int y = 0; y < dimension; y++) {
+            for (int x = 0; x < dimension; x++)
+                buffImg.setRGB(x, y, list.get(count ++).getRGB());
+        }
+
+        return buffImg;
+    }
+
+    /*************************************************
+     * Create Color
+     *************************************************/
+    private Color createColor(Integer r, Integer g, Integer b) {
+        return new Color(r, g, b, 0);
+    }
+
+    /*************************************************
+     * getters and setters
+     *************************************************/
+    public void setConfig(Pie_Config config) {
+        this.config = config;
+    }
+    public Pie_Config getConfig() {
+        return config;
+    }
+}
