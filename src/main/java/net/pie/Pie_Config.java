ebeb1eb7846d41ebe48089268c08dab84bb7c840
helcat1 <terry.clarke@gmail.com>
2023-09-12 16:54:20 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/net/pie/Start.java

--- src/main/java/net/pie/Pie_Config.java
+++ src/main/java/net/pie/Pie_Config.java
@@ -1,5 +1,44 @@
 package net.pie;
 
+import java.nio.charset.StandardCharsets;
+
 public class Pie_Config {
 
+    private int alpha = 0;
+    private boolean suppress_errors = false;
+    private int space = ("".getBytes(StandardCharsets.UTF_8)[0]);
+
+    public  Pie_Config() {
+
+    }
+
+
+    /*************************************
+     * getters and setters
+     *************************************/
+    public int getAlpha() {
+        return alpha;
+    }
+
+    public void setAlpha(int alpha) {
+        this.alpha = alpha;
+    }
+
+    public boolean isSuppress_errors() {
+        return suppress_errors;
+    }
+
+    public void setSuppress_errors(boolean suppress_errors) {
+        this.suppress_errors = suppress_errors;
+    }
+
+    public int getSpace() {
+        return space;
+    }
+
+    public void setSpace(int space) {
+        this.space = space;
+    }
 }
+
+
