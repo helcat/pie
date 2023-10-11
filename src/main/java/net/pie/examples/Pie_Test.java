85a1f63b26a31f1647685512b4ca1873313e7a08
terry <terry.clarke@gmail.com>
2023-10-11 09:21:20 +0100

    committed

--- src/main/java/net/pie/examples/Pie_Test.java
+++ src/main/java/net/pie/examples/Pie_Test.java
@@ -18,7 +18,6 @@ import java.util.logging.Level;
 import java.util.zip.Deflater;
 
 public class Pie_Test {
-
     private String temp_To_Be_Encoded = "background.jpg";
     private String temp_Encoded_Imaage = "My_Image.png";
     private String temp_Decode_To = "shared";
@@ -35,11 +34,13 @@ public class Pie_Test {
     public Pie_Test(String arg) {
         // Two numbers to combine
 // Two numbers to combine
-        int x = 64;
-        int y = 64;
+        int x = 0;
+        int y = 0;
 
 // Combine them into one
         int z = combine(x, y);
+        double test = z / 255;
+        System.out.println("combined / 255 = "+ test);
 
 // Print the combined number
         System.out.println("The combined number is " + z);
@@ -52,47 +53,6 @@ public class Pie_Test {
         System.out.println("The first number is " + x1);
         System.out.println("The second number is " + y1);
 
-        /**
-        // Build a config file for encoding.
-        Pie_Config encoding_config = new Pie_Config();
-        encoding_config.setLog_level(Level.INFO);                                                                   // Optional default is Level.SEVERE
-        encoding_config.setEncoder_Minimum_Image(new Pie_Size(0, 0, Pie_Constants.MIDDLE_CENTER));      // Optional default is 0,0, Pie_Position.MIDDLE_CENTER
-        encoding_config.setEncoder_Add_Encryption(false);                                                           // Optional default is true
-        encoding_config.setEncoder_Compression_Level(Deflater.BEST_COMPRESSION);                                    // Optional default is Deflater.BEST_SPEED
-        encoding_config.setEncoder_mode(Pie_Encode_Mode.ENCODE_MODE_ARGB);                                          // Optional default is Pie_Encode_Mode.ENCODE_MODE_ARGB
-        encoding_config.setShow_Memory_Usage_In_Logs(true);
-        encoding_config.setRun_gc_after(true);
-
-        // Build a Source File
-        Pie_Encode_Source source = new Pie_Encode_Source( encoding_config, new File(Pie_Utils.getDesktopPath() + File.separator + temp_To_Be_Encoded));
-
-        // Tell the encoder where to store the encoded image
-        Pie_Encoded_Destination encoded_destination = new Pie_Encoded_Destination();
-        encoded_destination.setLocal_file(new File(Pie_Utils.getDesktopPath() + File.separator + temp_Encoded_Imaage));
-
-        // Do Encoding, Will create the image and put it in the destination
-        new Pie_Encode(source, encoded_destination);     // Optional Config
-
-        //*******************************************************************
-
-        // Decoding - Decode the image created
-        Pie_Config decoding_config = new Pie_Config();
-        decoding_config.setLog_level(Level.INFO);
-        decoding_config.setShow_Memory_Usage_In_Logs(true);
-
-        // Tell the decoder where to store the decoded file
-        Pie_Decoded_Destination decoded_Source_destination = new Pie_Decoded_Destination();
-        decoded_Source_destination.setLocal_folder(new File( Pie_Utils.getDesktopPath() + File.separator + temp_Decode_To));
-
-        // Source file. (Image which was encoded)
-        Pie_Decode_Source decode_source = new Pie_Decode_Source(
-                decoding_config,
-                new File(Pie_Utils.getDesktopPath() + File.separator + temp_Encoded_Imaage + "." + Pie_Constants.IMAGE_TYPE.getParm2()));
-
-        // Do the decoding : Decodes the image.
-        new Pie_Decode(decode_source, decoded_Source_destination);
-
-         **/
     }
 
     // Combine two numbers into one
